d721a00ffc8b26cd7230746800bce9be840b9883
Daniel Stupakevich <stupakevich.daniel@gmail.com>
2019-12-16 18:12:43 +0300

    Add WhiteBear class

--- 1_Game/Enemies/Animals/Bear.cs
+++ 1_Game/Enemies/Animals/Bear.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace _1_Game.Enemies.Animals.ForestAnimals
 {
-    class Bear : Animal
+    public class Bear : Animal
     {
         public override int Strength { get; } = 2;
         public override int Speed { get; } = 10;
---
+++ 1_Game/Enemies/Animals/WhiteBear.cs
@@ -0,0 +1,17 @@
+﻿using _1_Game.Enemies.Animals.ForestAnimals;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1_Game.Enemies.Animals
+{
+    public class WhiteBear : Bear
+    {
+        public bool CanSwim { get; } = true;
+
+        public override void Roar()
+        {
+            Console.WriteLine("Рррыыыр! Я белый медведь!!");
+        }
+    }
+}
